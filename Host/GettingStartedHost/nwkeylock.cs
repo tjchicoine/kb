b059305a819c182c339b18316420339464b0a32e
Tyler Chicoine <tjchicoine@hotmail.com>
2018-08-29 17:48:41 -0600

    Port Opening

--- Host/GettingStartedHost/nwkeylock.cs
+++ Host/GettingStartedHost/nwkeylock.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.ServiceModel;
 using System.ServiceModel.Description;
+using NetFwTypeLib;
+
+
+
 
 namespace nwKeyboardLock
 {
@@ -11,6 +15,11 @@ namespace nwKeyboardLock
     {
         static void Main(string[] args)
         {
+            int port = 8080;
+            bool open = portcheck(port);
+            Console.WriteLine("Port " + port + " is " + ((open)? "open" : "closed"));
+            if (!open) { Fwr(); }
+
             Uri baseAddress = new Uri("http://192.168.100.139:8080/");
 
             // Create the ServiceHost.
@@ -35,6 +44,39 @@ namespace nwKeyboardLock
                 // Close the ServiceHost.
                 host.Close();
             }
+        }
+        static bool portcheck(int port)
+        {
+            bool exists = false;
+            Type tNetFwPolicy2 = Type.GetTypeFromProgID("HNetCfg.FwPolicy2");
+            INetFwPolicy2 fwPolicy2 = (INetFwPolicy2)Activator.CreateInstance(tNetFwPolicy2);
+            var currentProfiles = fwPolicy2.CurrentProfileTypes;
+            List<INetFwRule> RuleList = new List<INetFwRule>();
+            string i = Convert.ToString(port);
+            foreach (INetFwRule rule in fwPolicy2.Rules)
+            {
+                if (rule.LocalPorts == i)
+                {
+                    exists = true;
+                    return exists;
+                }
+            }
+            return exists;
+        }
+
+        static void Fwr()
+        {
+            INetFwRule firewallrule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            firewallrule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+            firewallrule.Description = ("Enables port 8080");
+            firewallrule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN;
+            firewallrule.Enabled = true;
+            firewallrule.InterfaceTypes = "All";
+            firewallrule.Name = "Test";
+            firewallrule.Protocol = (int)NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+            firewallrule.LocalPorts = "8080";
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            firewallPolicy.Rules.Add(firewallrule);
         }
     }
 }
